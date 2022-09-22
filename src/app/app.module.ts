35a6ed5929dcb249cb0c2f090645b9615ac8262c
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2022-09-22 16:29:33 -0300

    <feat> criação do service para trazer os dados para o componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
 //Modules
 import { PagesModule } from './pages/pages.module';
 
 //Components
-
+import { AppComponent } from './app.component';
 @NgModule({
   declarations: [
     AppComponent
@@ -16,7 +16,8 @@ import { PagesModule } from './pages/pages.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PagesModule
+    PagesModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
